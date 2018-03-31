163b59cbc5480e5ba6504966eb360d669a3d3213
Igor Laskaev <i.laskaev@gmail.com>
2018-03-31 09:32:53 -0400

    Added quill

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { AuthGuard } from './auth/auth.guard';
-import { ApplicationRouterModule } from './router/router.module';
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatProgressBarModule, MatProgressSpinnerModule, MatToolbarModule, MatSnackBarModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatProgressBarModule, MatProgressSpinnerModule, MatSnackBarModule, MatToolbarModule} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
+
 import {environment} from '../environments/environment';
 
 import {AppComponent} from './app.component';
 import {AuthComponent} from './auth/auth.component';
+import {AuthGuard} from './auth/auth.guard';
 import {AuthService} from './auth/auth.service';
 import {CoreModule} from './core/core.module';
 import {NavComponent} from './core/nav/nav.component';
-import { HomeComponent } from './home/home.component';
-import { ToastService } from './services/toast.service';
+import {QuillEditorModule} from './external/quill/quillEditor.module';
+import {HomeComponent} from './home/home.component';
+import {ApplicationRouterModule} from './router/router.module';
+import {ToastService} from './services/toast.service';
 
 
 @NgModule({
@@ -22,9 +24,10 @@ import { ToastService } from './services/toast.service';
   imports: [
     BrowserModule, CoreModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule, AngularFireAuthModule,  ApplicationRouterModule, MatToolbarModule,
-    MatButtonModule, MatProgressBarModule, MatMenuModule, MatIconModule, MatSnackBarModule,
-    MatProgressSpinnerModule, MatCardModule
+    AngularFirestoreModule, AngularFireAuthModule, ApplicationRouterModule,
+    MatToolbarModule, MatButtonModule, MatProgressBarModule, MatMenuModule,
+    MatIconModule, MatSnackBarModule, MatProgressSpinnerModule, MatCardModule,
+    QuillEditorModule
   ],
   providers: [AuthService, AuthGuard, ToastService],
   bootstrap: [AppComponent]
