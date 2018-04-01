5cbef95b373c6d52dfa4c62284204e7397901ad8
Jesse-Daniel Belleau-Karaskewich <jb15iq@brocku.ca>
2018-04-01 03:42:23 -0400

    Services Created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatProgressBarModule, MatProgressSpinnerModule, MatSnackBarModule, MatToolbarModule} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
@@ -18,7 +19,8 @@ import {QuillEditorModule} from './external/quill/quillEditor.module';
 import {HomeComponent} from './home/home.component';
 import {ApplicationRouterModule} from './router/router.module';
 import {ToastService} from './services/toast.service';
-
+import {SpeechRecognitionService} from './services/speech-recognition.service';
+import {SummaryService} from './services/summary.service';
 
 
 @NgModule({
@@ -29,9 +31,10 @@ import {ToastService} from './services/toast.service';
     AngularFirestoreModule, AngularFireAuthModule, ApplicationRouterModule,
     MatToolbarModule, MatButtonModule, MatProgressBarModule, MatMenuModule,
     MatIconModule, MatSnackBarModule, MatProgressSpinnerModule, MatCardModule,
-    QuillEditorModule
+    QuillEditorModule, HttpClientModule
   ],
-  providers: [AuthService, AuthGuard, ToastService],
+  providers: [AuthService, AuthGuard, ToastService, SpeechRecognitionService, 
+    SummaryService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ client/src/app/services/speech-recognition.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import Recorder from 'recorder-js';
+
+declare const webkitSpeechRecognition; // For built in speech recognition
+
+@Injectable()
+export class SpeechRecognitionService {
+  private speechRecognition: any;
+  public isRecording = false;
+  private speechRecognitionObservable: Observable<string>;
+  private finalizedText = "";
+  private currentResultIndex = 0; // Keeps track of the results of the speech stream
+  private restartInterval;
+
+  constructor() { 
+    this.speechRecognition = new webkitSpeechRecognition();
+  }
+
+  /**
+   * Returns an observable that yields a result every time some speech is recognized.
+   * The string returned is the string from the entire duration of the speech recognition
+   * 
+   * @param {boolean} continous If true, the speech recognition continues indefinitely
+   * @param {boolean} interimResults If true, results are returned as they are parsed, and are subject to change
+   * @param {string} language Language code for the speech recognition (e.g. 'en-US')
+   * @returns {Observable<string>} 
+   * 
+   * @memberOf SpeechRecognitionService
+   */
+  public startSpeechRecognition(continuous: boolean, interimResults: boolean, language: string): Observable<string> {
+    if (this.isRecording) { // If we are already recording, returnt that observable
+      return this.speechRecognitionObservable;
+    }
+    this.isRecording = true;
+    this.speechRecognition.continuous = continuous;
+    this.speechRecognition.interimResults = interimResults;
+    this.speechRecognition.lang = language;
+    this.finalizedText = "";
+
+    // If the speech recognition ends randomly, restart it
+    this.speechRecognition.onend = () => {
+      this.speechRecognition.start();
+    };
+
+    this.speechRecognition.onstart = () => {
+      this.currentResultIndex = 0;
+    };
+
+    this.speechRecognition.start();
+    this.restartInterval = setInterval(() => {
+      if (this.isRecording) {
+        this.speechRecognition.stop();
+      }
+    }, 30000);
+
+    return Observable.create((observer) => {
+      this.speechRecognition.onresult = (e) => {
+        let newText;
+        if (e.resultIndex != this.currentResultIndex) {
+          newText = (e.results[this.currentResultIndex][0].transcript + ".").trim();
+          newText = " " + newText.trim().charAt(0).toUpperCase() + newText.slice(1);
+          this.finalizedText += newText;
+          this.currentResultIndex = e.resultIndex;
+        }
+        newText = e.results[e.resultIndex][0].transcript.trim();
+        newText = " " + newText.trim().charAt(0).toUpperCase() + newText.slice(1);
+        return observer.next(this.finalizedText + newText);
+      }
+    });
+  }
+
+  /**
+   * Stops the speech recognition and returns the final text value
+   * 
+   * @returns {string} 
+   * 
+   * @memberOf SpeechRecognitionService
+   */
+  public stopSpeechRecognition(): string {
+    this.isRecording = false;
+    clearInterval(this.restartInterval);
+    this.speechRecognition.onend = (e) => {};
+    this.speechRecognition.stop();
+    return this.finalizedText;
+  }
+}
